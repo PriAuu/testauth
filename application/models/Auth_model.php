b8a8bae9cb3765dfa4e8de9479b58e0d65248756
Danupong55 <151014296+Danupong55@users.noreply.github.com>
2024-01-14 21:59:04 +0700

    a

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -3,16 +3,19 @@ class Auth_model extends CI_Model{
     
     public function check_login($email, $password)
     {
+        //check db
         $query = $this->db->query('SELECT * FROM users');
         
+        //hash password
         $hash_password = md5($password);
         
+        //loop
         foreach($query->result() as $row){
             if($row->email == $email && $row->password == $hash_password){
                 return true; 
             }
         }
-
+        //return
         return false;
     }
 }
